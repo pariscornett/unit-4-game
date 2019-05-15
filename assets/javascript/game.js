42f476048ce7abe250eedb99320b881989a2c332
Paris Cornett <pariscornett@Pariss-MacBook-Air.local>
2019-05-15 13:05:41 -0500

    generated random number to guess and displayed in DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,21 @@
 //set global variables
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+var numberToGuess;
+var basil;
+var clove;
+var dill;
+var sage;
 
 
+//randomize the number for the player to guess (set to 1-10 for testing purposes)
 
-//randomize the number for the player to guess
+var numberToGuess = 1 + Math.floor(Math.random() * 10);
 
 
 //display that number for the user to see
-
+$(".randomNumber").append(numberToGuess);
 
 //randomize the number for each icon
 
