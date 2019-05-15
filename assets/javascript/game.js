a453194d9a51179d81e3885553ee8c3a15e77250
Paris Cornett <pariscornett@Pariss-MacBook-Air.local>
2019-05-15 12:51:06 -0500

    added pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+//set global variables
+
+
+
+//randomize the number for the player to guess
+
+
+//display that number for the user to see
+
+
+//randomize the number for each icon
+
+
+//code randomized numbers to add up 
+
+
+//store added numbers in score, display in DOM
+
+
+
+//set conditions for winning and losing
+
+
+
+
+
+//record wins and losses in DOM
+
+
+
+
+//make game reset with wins and losses without refreshing page
+
+
+//when game resets, reset randomized number for user to guess && re-randomize the icon numbers
