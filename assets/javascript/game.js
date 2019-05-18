0caf4a88d82f339096cf692ecf2548774fd828ad
Paris Cornett <pariscornett@Pariss-MacBook-Air.local>
2019-05-18 13:44:48 -0500

    fixed issue resetting game and displaying wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 //set global variables
 var wins = 0;
 var losses = 0;
@@ -15,54 +16,78 @@ var numberToGuess = 1+ Math.floor(Math.random() * 10); //19*100 in future
 
 
 //display that number for the user to see
-$(".randomNumber").append(numberToGuess);
+$("#targetNumber").html(numberToGuess);
 
-//randomize the number for each icon (set tp 1-2 for testing purposes)
 var basilClick = 1 + Math.floor(Math.random()* 2);  //1*12 in future  
 var cloveClick = 1 + Math.floor(Math.random()* 2);
 var dillClick = 1 + Math.floor(Math.random()* 2);
 var sageClick = 1 + Math.floor(Math.random()* 2);
 
+//randomize the number for each icon (set tp 1-2 for testing purposes)
+function resetGame() {
+basilClick = 1 + Math.floor(Math.random()* 2);  //1*12 in future  
+cloveClick = 1 + Math.floor(Math.random()* 2);
+dillClick = 1 + Math.floor(Math.random()* 2);
+sageClick = 1 + Math.floor(Math.random()* 2);
+numberToGuess = 1+ Math.floor(Math.random() * 10);
+$("#targetNumber").html(numberToGuess);
+totalScore = 0;
+}
+
+function update() {
+    if (totalScore == numberToGuess) {
+        wins++
+        //record wins and losses in DOM
+        $(".wins").html("Wins = " + wins);
+        resetGame();
+    } else if (totalScore > numberToGuess){
+        losses++
+        //record wins and losses in DOM
+        $(".losses").html("Losses = " + losses);
+        resetGame();
+    }
+}
+
+
 //make icons clickable
-$(".basil").click(function() {
+$(".basil").click(function(){
+    console.log (totalScore, numberToGuess)
     //add up score and store it in the var totalScore
    totalScore += basilClick;
     //erase previous score from DOM
     $(".score").empty();
     //display current score
     $(".score").append("Your Score: " +totalScore);
+    //call function update
+    update();
 });
+
 $(".clove").click(function() {
+    console.log (totalScore, numberToGuess)
    totalScore += cloveClick;
     $(".score").empty();
     $(".score").append("Your Score: " +totalScore);
+    update();
 });
 
 $(".dill").click(function() {
+    console.log (totalScore, numberToGuess)
    totalScore += dillClick;
     $(".score").empty();
     $(".score").append("Your Score: " +totalScore);
+    update();
 });
 
 $(".sage").click(function() {
+    console.log (totalScore, numberToGuess)
    totalScore += sageClick;
     $(".score").empty();
     $(".score").append("Your Score: " +totalScore);
+    update();
 });
-
-
-//set conditions for winning and losing
-$(document).ready(function(){
-    if (totalScore === numberToGuess) {
-        wins++
-        //record wins and losses in DOM
-    } else if (totalScore > numberToGuess){
-        losses++
-        //record wins and losses in DOM
-        $(".losses").html(losses);
     }
+);
 
-});
 
 
 
@@ -73,4 +98,4 @@ $(document).ready(function(){
 //make game reset with wins and losses without refreshing page
 
 
-//when game resets, reset randomized number for user to guess && re-randomize the icon numbers
+//when game resets, reset randomized number for user to guess && re-randomize the icon 
